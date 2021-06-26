d15d3b93eeff0b2f1cfcaac054da3cea49cf6722
surey07 <86525933+surey07@users.noreply.github.com>
2021-06-26 20:47:05 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Welcome to our website</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Surendhar raaj</h1>
+      <p>Specialist in Data analyist and web developer.</p>
+    </div>
+  );
+}
+
+export default Info;
